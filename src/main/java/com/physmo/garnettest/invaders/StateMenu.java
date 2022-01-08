8e7bfbecffe46f3ca15af2e5dac1dc5fe9a4f163
Nick Donnelly <nickd3000@hotmail.com>
2022-01-08 23:07:24 +0000

    Adding bitmap font support - angelcode etc

--- src/main/java/com/physmo/garnettest/invaders/StateMenu.java
+++ src/main/java/com/physmo/garnettest/invaders/StateMenu.java
@@ -2,14 +2,21 @@ package com.physmo.garnettest.invaders;
 
 import com.physmo.garnet.GameState;
 import com.physmo.garnet.Garnet;
+import com.physmo.garnet.Texture;
 import com.physmo.garnet.Utils;
+import com.physmo.garnet.bitmapfont.BMFFont;
 import com.physmo.garnet.input.Input;
 import com.physmo.garnet.regularfont.RegularFont;
+
+import java.io.IOException;
 
 public class StateMenu extends GameState {
 
     public static final String fontName = "/12x12Font.png";
     RegularFont regularFont;
+
+    BMFFont bmfFont;
+    Texture bmfFontTexture;
 
     public StateMenu(Garnet garnet, String name) {
         super(garnet, name);
@@ -19,6 +26,16 @@ public class StateMenu extends GameState {
     public void init(Garnet garnet) {
         String fontFileName = Utils.getPathForResource(this, fontName);
         regularFont = new RegularFont(fontFileName, 12, 12);
+
+        bmfFontTexture = Texture.loadTexture(Utils.getPathForResource(this, "/defaultfont.png"));
+        bmfFont = new BMFFont();
+        String pathForResource = Utils.getPathForResource(this, "/defaultfont.fnt");
+        try {
+            bmfFont.init(pathForResource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -33,5 +50,11 @@ public class StateMenu extends GameState {
         regularFont.clearSpriteBatch();
         regularFont.drawText("Invaders", 20, 100, 3);
         regularFont.render();
+
+        // temp bmf font stuff
+        bmfFontTexture.bind();
+        bmfFont.drawString(bmfFontTexture, "Hello World 123", 20, 20, 1);
+
+        ////////
     }
 }
