1b5ad2161118638a8c4517afe4ebf44a73833169
Nick Donnelly <nickd3000@hotmail.com>
2023-03-29 22:24:14 +0100

    Adding tiling system

---
+++ src/main/java/com/physmo/garnetexamples/graphics/TileGridExample.java
@@ -0,0 +1,91 @@
+package com.physmo.garnetexamples.graphics;
+
+import com.physmo.garnet.Garnet;
+import com.physmo.garnet.GarnetApp;
+import com.physmo.garnet.Texture;
+import com.physmo.garnet.Utils;
+import com.physmo.garnet.graphics.Graphics;
+import com.physmo.garnet.spritebatch.TileSheet;
+import com.physmo.garnet.tilegrid.TileGridData;
+import com.physmo.garnet.tilegrid.TileGridDrawer;
+
+import java.util.Random;
+
+import static org.lwjgl.opengl.GL11.GL_SCISSOR_TEST;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glScissor;
+
+// NOTE: on MacOS we need to add a vm argument: -XstartOnFirstThread
+public class TileGridExample extends GarnetApp {
+
+    String imageFileName = "space.png";
+    TileSheet tileSheet;
+    Texture texture;
+    Graphics graphics;
+    double x = 0;
+
+    TileGridDrawer tileGridDrawer;
+    TileGridData tileGridData;
+    Random random = new Random();
+
+    public TileGridExample(Garnet garnet, String name) {
+        super(garnet, name);
+    }
+
+    public static void main(String[] args) {
+        Garnet garnet = new Garnet(400, 400);
+        GarnetApp app = new TileGridExample(garnet, "");
+
+        garnet.setApp(app);
+
+        garnet.init();
+        garnet.run();
+    }
+
+    @Override
+    public void init(Garnet garnet) {
+        texture = Texture.loadTexture(Utils.getPathForResource(this, imageFileName));
+        tileSheet = new TileSheet(texture, 16, 16);
+        graphics = garnet.getGraphics();
+        graphics.addTexture(texture);
+
+        int mapWidth = 100;
+        int mapHeight = 100;
+
+        tileGridData = new TileGridData(mapWidth, mapHeight);
+        tileGridDrawer = new TileGridDrawer()
+                .setData(tileGridData)
+                .setWindowSize(5, 5)
+                .setTileSize(16, 16)
+                .setTileSheet(tileSheet);
+
+
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                tileGridData.setTileId(x, y, random.nextInt(5));
+            }
+        }
+    }
+
+    @Override
+    public void tick(double delta) {
+        x += delta * 50;
+        if (x > 200) x = 0;
+
+        tileGridDrawer.setScroll(x, x / 2);
+    }
+
+    @Override
+    public void draw() {
+//        graphics.setColor(Color.GREEN.toInt());
+//        graphics.setScale(5);
+//        graphics.drawImage(tileSheet, (int) x, 5, 2, 2);
+
+        glEnable(GL_SCISSOR_TEST);
+        glScissor(0, 0, 100, 200);
+        tileGridDrawer.draw(graphics, 20, 20);
+
+        graphics.render();
+    }
+
+}
