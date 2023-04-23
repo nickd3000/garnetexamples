0c9c49097d919a07425d5f210a2eaa7d89283650
Nick Donnelly <nickd3000@hotmail.com>
2023-04-23 09:48:48 +0100

    Added constants class

---
+++ src/main/java/com/physmo/garnetexamples/cellsurvivor/Constants.java
@@ -0,0 +1,9 @@
+package com.physmo.garnetexamples.cellsurvivor;
+
+public class Constants {
+    public static final String TAG_CRYSTAL = "crystal";
+    public static final String TAG_PLAYER = "player";
+    public static final String TAG_ENEMY = "enemy";
+    public static final String TAG_BULLET = "bullet";
+
+}
