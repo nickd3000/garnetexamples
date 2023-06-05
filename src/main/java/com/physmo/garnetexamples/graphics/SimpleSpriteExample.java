70726aaef4af68d09b1eab671e8aa9b9d0c82679
Nick Donnelly <nickd3000@hotmail.com>
2023-06-05 01:44:11 +0100

    Simplified sprite example

--- src/main/java/com/physmo/garnetexamples/graphics/SimpleSpriteExample.java
+++ src/main/java/com/physmo/garnetexamples/graphics/SimpleSpriteExample.java
@@ -3,56 +3,39 @@ package com.physmo.garnetexamples.graphics;
 import com.physmo.garnet.Garnet;
 import com.physmo.garnet.GarnetApp;
 import com.physmo.garnet.Texture;
-import com.physmo.garnet.drawablebatch.TileSheet;
 import com.physmo.garnet.graphics.Graphics;
 
 // NOTE: on MacOS we need to add a vm argument: -XstartOnFirstThread
 public class SimpleSpriteExample extends GarnetApp {
 
-    TileSheet tileSheet;
     Texture texture;
-    double xPos = 0;
-    double scale = 4;
 
     public SimpleSpriteExample(Garnet garnet, String name) {
         super(garnet, name);
     }
 
     public static void main(String[] args) {
-        Garnet garnet = new Garnet(400, 400);
+        Garnet garnet = new Garnet(600, 400);
         GarnetApp app = new SimpleSpriteExample(garnet, "");
-
         garnet.setApp(app);
-
         garnet.init();
         garnet.run();
     }
 
     @Override
     public void init(Garnet garnet) {
-        texture = Texture.loadTexture("space.png");
-        tileSheet = new TileSheet(texture, 16, 16);
-        Graphics graphics = garnet.getGraphics();
-        graphics.addTexture(texture);
+        texture = Texture.loadTexture("garnetCrystal.png");
+        garnet.getGraphics().addTexture(texture);
     }
 
     @Override
     public void tick(double delta) {
-        xPos += delta * 50;
-        if (xPos > 80) xPos = -16;
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setScale(scale);
-
-        int[] mousePosition = garnet.getInput().getMousePositionScaled(scale);
-
-        g.setColor(0xaaff22ff);
-        g.drawImage(tileSheet, (int) xPos, 5, 2, 2);
-
-        g.setColor(0xaa22ffff);
-        g.drawImage(tileSheet, mousePosition[0], mousePosition[1], 2, 2);
+        int[] mousePosition = garnet.getInput().getMousePosition();
+        g.drawImage(texture, mousePosition[0], mousePosition[1]);
     }
 }
 
